9c5afa23a0e9d6b39131adc40777f3064afc4331
timvisee <tim@visee.me>
2020-02-13 18:59:28 +0100

    Put X11 fork and bin providers behind feature toggle, organize exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,16 @@
-pub mod combined;
+mod combined;
 #[cfg(all(
+    feature = "x11-bin",
     unix,
     not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
 ))]
 pub mod x11_bin;
 #[cfg(all(
+    feature = "x11-fork",
     unix,
     not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
 ))]
 pub mod x11_fork;
 
-// Re-export clipboard
 pub use clipboard;
-
-// Re-export
 pub use combined::CombinedClipboardContext;
-#[cfg(all(
-    unix,
-    not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
-))]
-pub use x11_bin::X11BinClipboardContext;
-#[cfg(all(
-    unix,
-    not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
-))]
-pub use x11_fork::X11ForkClipboardContext;
