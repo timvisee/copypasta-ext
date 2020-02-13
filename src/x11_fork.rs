89111258642e715c2dda192ba4c673bae5aadbfb
timvisee <tim@visee.me>
2020-02-13 16:51:29 +0100

    Keep X11ForkClipboardProvider fork alive until clipboard changes

--- src/x11_fork.rs
+++ src/x11_fork.rs
@@ -1,12 +1,29 @@
 use std::error::Error as StdError;
 use std::fmt;
-use std::marker::PhantomData;
 
 use clipboard::x11_clipboard::{Clipboard, Selection, X11ClipboardContext};
-use clipboard::{ClipboardContext, ClipboardProvider};
+use clipboard::ClipboardProvider;
 use libc::fork;
+use x11_clipboard::Clipboard as X11Clipboard;
 
-pub struct X11ForkClipboarContext<S = Clipboard>(X11ClipboardContext<S>, PhantomData<S>)
+/// Like [`X11ClipboardContext`][X11ClipboardContext], but forks to set contents.
+///
+/// This provider ensures the clipboard contents you set remain available even after your
+/// application exists, unlike [`X11ClipboardContext`][X11ClipboardContext].
+///
+/// When setting the clipboard with `set_contents`, the process is forked in which the clipboard is
+/// set. The fork is kept alive until the clipboard content changes, and may outlive your
+/// application.
+///
+/// ## Drawbacks
+///
+/// - Set contents may not be immediately available, because they are set in a fork.
+/// - Errors when setting the clipboard contents are not catched, the fork will panic
+///   `set_contents` will return no error.
+/// - The fork might cause weird behaviour for some applications.
+///
+/// [X11ClipboardContext]: ../../clipboard/x11_clipboard/struct.X11ClipboardContext.html
+pub struct X11ForkClipboarContext<S = Clipboard>(X11ClipboardContext<S>)
 where
     S: Selection;
 
@@ -15,7 +32,7 @@ where
     S: Selection,
 {
     fn new() -> Result<Self, Box<dyn StdError>> {
-        Ok(Self(X11ClipboardContext::<S>::new()?, PhantomData))
+        Ok(Self(X11ClipboardContext::new()?))
     }
 
     fn get_contents(&mut self) -> Result<String, Box<dyn StdError>> {
@@ -26,11 +43,23 @@ where
         match unsafe { fork() } {
             -1 => Err(Error::Fork.into()),
             0 => {
-                let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-                ctx.set_contents(contents).unwrap();
+                // Obtain new X11 clipboard context, set clipboard contents
+                let clip = X11Clipboard::new().expect("failed to obtain X11 clipboard context");
+                clip.store(
+                    S::atom(&clip.setter.atoms),
+                    clip.setter.atoms.utf8_string,
+                    contents,
+                )
+                .expect("failed to set clipboard contents through forked process");
 
-                // TODO: wait for clipboard change instead
-                std::thread::sleep(std::time::Duration::from_secs(60));
+                // Wait for clipboard to change, then kill fork
+                clip.load_wait(
+                    S::atom(&clip.getter.atoms),
+                    clip.getter.atoms.utf8_string,
+                    clip.getter.atoms.property,
+                )
+                .expect("failed to wait on new clipboard value in forked process");
+
                 std::process::exit(0)
             }
             _pid => Ok(()),
